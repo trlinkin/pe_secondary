5b95d198ea947eef8cf896ee71f35b6c018c3267
Thomas Linkin <trlinkin@gmail.com>
2013-10-29 15:11:24 -0400

    Add management for console database settings

---
+++ manifests/console/database.pp
@@ -0,0 +1,14 @@
+class pe_secondary::console::database(
+  $database = 'console',
+  $username = 'console',
+  $host     = 'localhost',
+  $port     = '5432',
+  $adapter  = 'postgresql',
+  $password,
+){
+
+  file { '/etc/puppetlabs/puppet-dashboard/database.yml',
+    ensure  => file,
+    content => template('pe_secondary/database.yml.erb'),
+  }
+}
