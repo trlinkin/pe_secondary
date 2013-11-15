b10a974c861b76e4b1c9a091aaee8220a4d893a0
Thomas Linkin <trlinkin@gmail.com>
2013-11-15 11:27:28 -0500

    Refactor Event inspcetor config to use template
    This commit refactored event inspector config management to use a
    teamplate. Since it is not a big file, the use of file_line was
    uneccessary and overly complicated. Along the way I was able to add more
    support for other settings too.

--- manifests/console/event_inspector.pp
+++ manifests/console/event_inspector.pp
@@ -1,26 +1,15 @@
 class pe_secondary::console::event_inspector(
-  $console_name = $::fqdn,
+  $console_name  = $::pe_secondary::params::console_name,
+  $puppetdb_port = $::pe_secondary::params::puppetdb_port,
+  $puppetdb_host = $::fqdn,
 ){
 
-  file_line { 'event_inspector_ca_file':
-    ensure => present,
-    line   => "    ca_file: /opt/puppet/share/puppet-dashboard/certs/pe-internal-dashboard-${console_name}.ca_cert.pem",
-    match  => '^\s*ca_file:',
-    path   => '/opt/puppet/share/event-inspector/config/config.yml',
-  }
-
-  file_line { 'event_inspector_cert_file':
-    ensure => present,
-    line   => "    cert: /opt/puppet/share/puppet-dashboard/certs/pe-internal-dashboard-${console_name}.cert.pem",
-    match  => '^\s*cert:',
-    path   => '/opt/puppet/share/event-inspector/config/config.yml',
-  }
-
-  file_line { 'event_inspector_key_file':
-    ensure => present,
-    line   => "    key: /opt/puppet/share/puppet-dashboard/certs/pe-internal-dashboard-${console_name}.private_key.pem",
-    match  => '^\s*key:',
-    path   => '/opt/puppet/share/event-inspector/config/config.yml',
+  file { 'pe_event_inspector_config':
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    content => template('pe_secondary/event_inspector_config.yml.erb'),
   }
 }
 
---
+++ manifests/params.pp
@@ -0,0 +1,6 @@
+class pe_secondary::params {
+  $console_name = $::fqdn
+
+  $puppetdb_port = '8081'
+  $puppet_port   = '8140'
+}
